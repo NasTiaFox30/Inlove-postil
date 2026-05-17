90fcfb967f07828ef84208c52701c11da6bc8c96
Anastasiia Bzova <bzovanastia@gmail.com>
2026-05-17 14:41:06 +0200

    Adde useState (ContactSection.jsx)

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styles from './ContactSection.module.css'
 
 export default function ContactSection({ cartList, onClearCart }) {
